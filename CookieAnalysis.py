6ca0988942b7750bcb1301379b26cfd60ea8c473
Stephanie Toole <122947637+StephToo@users.noreply.github.com>
2023-02-01 17:08:09 -0600

    Update CookieAnalysis.py
    hw 4 added quantiles and plot (histogram)

--- CookieAnalysis.py
+++ CookieAnalysis.py
@@ -1,14 +1,15 @@
 #! /usr/bin/env python
 
-# imports of external packages to use in our code
+# imports of external packages to use in our code 
 import sys
 import math
 import numpy as np
 import matplotlib.pyplot as plt
 
-# import our Random class from python/Random.py file
+#import our Random class from /Random.py file
 sys.path.append(".")
-from python.MySort import MySort
+from MySort import MySort
+#import sorting
 
 # main function for our CookieAnalysis Python code
 if __name__ == "__main__":
@@ -60,5 +61,33 @@ if __name__ == "__main__":
     # times_avg = Sorter.BubbleSort(times_avg)
     # times_avg = Sorter.InsertionSort(times_avg)
     # times_avg = Sorter.QuickSort(times_avg)
+   
 
-    # ADD YOUR CODE TO PLOT times AND times_avg HERE
+
+    # Code to calculate different quantiles from sorted arrays of outcomes
+    # numpy.quantile() method
+ 
+    # 1D array
+    arr = [20, 2, 7, 1, 34]
+ 
+    print("Q2 quantile of arr : ", np.quantile(times, .50))
+    print("Q1 quantile of arr : ", np.quantile(arr, .25))
+    print("Q3 quantile of arr : ", np.quantile(arr, .75))
+    print("100th quantile of arr : ", np.quantile(arr, .1))
+
+
+
+  
+    fig1 = plt.figure()
+    # plt.plot(times,p,'o-')
+    plt.hist(times_avg, density=True, facecolor='b', alpha=0.5, label="assuming $\\mathbb{H}_0$")
+    # plt.plot(sizes,[0.0248]*len(sizes),'--r')
+    plt.grid()
+    plt.xlim(xmin=0)
+    plt.xlabel('Time')
+    plt.ylabel('Frequency of Outcomes')
+    plt.title('Distribution of Outcomes from Input File')
+
+    plt.show()
+
+    fig1.savefig('cookieplot.png')
